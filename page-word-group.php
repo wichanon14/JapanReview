dc5eb144412bb4c00066c5e5b8a9b1fd5f658500
wichanon14 <wichanon17978@gmail.com>
2019-05-04 03:33:45 +0700

    update

--- page-word-group.php
+++ page-word-group.php
@@ -162,18 +162,50 @@
         <button class="navbar-toggler mt-minus-2" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>        
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse bg-dark " id="navbarNav">
+            <ul class="navbar-nav mr-auto bg-dark ml-4 mr-4">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Home </a>
+                </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="#">Learning Journey <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Learning Journey</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">Practice Journey</a>
                 </li>
             </ul>
+        </div>
+
+        <!-- Sign In Module -->
+        <div class="nav-item dropdown my-2 my-lg-0">
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Sign In
+            </a>
+            <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink" style="width:21em;margin-left:-15em;">
+                <div class="row mt-3"></div>
+                <div class="row">
+                    <div class="col-sm-4 mt-1 ml-4 mr-1 text-light">
+                        <span>Username :</span>
+                    </div>
+                    <div class="col-sm-6">
+                        <input class="form-control" type="input" />
+                    </div>
+                </div>
+                <div class="row mt-3"></div>
+                <div class="row">
+                    <div class="col-sm-4 mt-1 ml-4 mr-1 text-light">
+                        <span>Password :</span>
+                    </div>
+                    <div class="col-sm-6">
+                        <input class="form-control" type="password" />
+                    </div>
+                </div>
+                <div class="row mt-3" style="margin-left:63%;">
+                    <div class="col-sm-5">
+                        <button class="btn btn-outline-light" type="button">Sign In</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </nav>
@@ -199,7 +231,7 @@
                     <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
                 </div>
             </div>
-            <div id="group-manage-addword" class="col-sm-6 mt-3 hide" >
+            <div id="group-manage-addword insert" class="col-sm-6 mt-3 hide" >
                 <div class="row  ml-1">
                     <h2>Word Grouping</h2>
                     <div class="text-danger ml-3 mt-2 hide font-weight-bold" id="add_status">
