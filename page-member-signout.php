f258e9dca808f96cda68de3cf84db57e108b9c88
wichanon14 <wichanon17978@gmail.com>
2019-05-06 16:33:06 +0700

    Update page-member-signout.php

--- page-member-signout.php
+++ page-member-signout.php
@@ -1,9 +1,9 @@
 <?php 
     session_start();
 
-    require_once('/Service/All_php_function.php');
+    require_once('./Service/All_php_function.php');
     recordLogs($conn,$_SESSION['user-id'],'Sign Out');
-    
+
     header('Location: ./page-home.php');
     session_destroy();
 ?>
